e901178c28e86982753c4f9838525a62cad55b46
Wuizlz <brisenodan12@gmail.com>
2025-06-20 13:01:27 -0500

    added useEffect

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-//import './index.css';
-//import App from './App';
+import './index.css';
+import App from './App';
 
-import StarRating from "./starRating";
+//import StarRating from "./starRating";
 
-function Test() {
+{/*function Test() {
   const [movieRating, setMovieRating] = useState(0);
   return <div>
     <StarRating color='blue' onSetRating = {setMovieRating}></StarRating>
     <p>This movie was rated {movieRating} stars</p>
   </div>;
-}
+}*/}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/*  <App /> */}
-    <StarRating
+      <App /> 
+    {/*<StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     ></StarRating>
@@ -27,6 +27,6 @@ root.render(
       className="test"
       defaultRating={3}
     ></StarRating>
-    <Test />
+    <Test />*/}
   </React.StrictMode>
 );
